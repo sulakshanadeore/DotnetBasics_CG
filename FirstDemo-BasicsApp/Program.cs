e0810d446911374d3a053c796c27eb9ced12c1f6
deoresulakshana <deoresulakshana@gmail.com>
2024-11-05 15:01:43 +0530

    Add project files.

---
+++ FirstDemo-BasicsApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LogicLibraryForCalculator;
+
+namespace FirstDemo_BasicsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //User Interface
+
+            //int empid=Employee.GenerateEmployeeID();
+            //Console.WriteLine(empid);
+            //empid = Employee.GenerateEmployeeID();
+            //Console.WriteLine(empid);
+            //empid = Employee.GenerateEmployeeID();
+            //Console.WriteLine(empid);
+            //empid = Employee.GenerateEmployeeID();
+            //Console.WriteLine(empid);
+            //empid = Employee.GenerateEmployeeID();
+            //Console.WriteLine(empid);
+            //empid = Employee.GenerateEmployeeID();
+            //Console.WriteLine(empid);
+
+            Employee emp = new Employee();
+            Console.WriteLine("Enter employeename");
+            string _ename=Console.ReadLine();
+            Console.WriteLine("Enter city");
+            string _city=Console.ReadLine();
+            Console.WriteLine("Enter sal");
+            float _sal=Convert.ToSingle(Console.ReadLine());
+            emp.AcceptEmpDetails(_ename, _city, _sal);
+           
+            
+            emp.ShowEmpDetails();
+
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LogicLibraryForCalculator/SimpleCalculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLibraryForCalculator
+{
+    public class SimpleCalculator
+    {
+       public static int numberOfCalcuationsDone = 0;
+        public static string Greet()
+        {
+            numberOfCalcuationsDone = numberOfCalcuationsDone + 1;
+            string s = "Greetings of the day!!";
+            return s;
+
+        }
+
+
+
+        //public int AddTwoNos(int fno, int sno) { 
+        
+        
+        //}
+
+        //public int SubtractTwoNos(int fno, int sno)
+        //{
+
+
+        //}
+
+        //public double SubtractOfTwoNos(int fno, int sno,float tno)
+        //{
+
+
+        //}
+
+
+
+        //public long MultiplyTwoNos(int fno, int sno)
+        //{
+
+
+        //}
+        //public int DivideTwoNos(int fno, int sno)
+        //{
+
+
+        //}
+        //Logic
+    }
+}
